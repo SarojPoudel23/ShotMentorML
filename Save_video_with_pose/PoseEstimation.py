54152bfa4e3dbf3fc2894b8661d4a7a149585b5c
Saroj Poudel <saroj.poudel@outcodesoftware.com>
2024-01-12 17:00:38 +0545

    Videos to (Videos with skeleton)

---
+++ Save_video_with_pose/PoseEstimation.py
@@ -0,0 +1,34 @@
+import cv2
+from PoseModule import poseDetector
+
+def main():
+    cap = cv2.VideoCapture('Videos/20.mp4')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    original_fps = cap.get(cv2.CAP_PROP_FPS)
+
+    out = cv2.VideoWriter('y.mp4', fourcc, original_fps, (406, 720))
+
+    detector = poseDetector()
+
+    while True:
+        success, img = cap.read()
+
+        if not success:
+            print("Error: Failed to capture a frame.")
+            break
+        if not img.size == 0:
+            resized_img = cv2.resize(img, (406, 720))
+
+            img = detector.findPose(resized_img)
+
+            out.write(img)
+        # cv2.imshow("Image", resized_img)
+
+        # cv2.waitKey(20)
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
